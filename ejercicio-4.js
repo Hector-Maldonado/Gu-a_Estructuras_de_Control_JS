e31a4c2bdddfa118d0b5ba073463d42995d8c4fe
Hector-Maldonado <maldonadohector54@gmail.com>
2024-09-07 23:04:22 -0600

    Ejercicio 4

---
+++ ejercicio-4.js
@@ -0,0 +1,22 @@
+//Ejercicio #4
+/*
+Crear una función que en base a 2 números enteros que ingrese el usuario,
+calcular cual número es el mayor y devolverlo.
+*/
+function numeroMayor(num1, num2) {
+
+
+    if (num1 > num2) {
+  
+      console.log("el numero mayor es: " + num1);
+  
+    } else if (num2 > num1) {
+      console.log("el numero mayor es: " + num2);
+  
+    }
+  
+  
+  }
+  
+  numeroMayor(1, 10)
+  
