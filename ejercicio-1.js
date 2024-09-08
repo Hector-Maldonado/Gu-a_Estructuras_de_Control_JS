41bf521170ec52728fd90c5f53462d2d83b36289
Hector-Maldonado <maldonadohector54@gmail.com>
2024-09-07 23:02:25 -0600

    Ejercicio 1

---
+++ ejercicio-1.js
@@ -0,0 +1,12 @@
+//Ejercicio #1
+/* 
+Crear una función que en base a la edad que ingreso el usuario devolver un
+mensaje si la persona es mayor de edad o no. Utilizar para la condición el operador ternario.
+*/
+function mayorEdad(edad) {
+    edad > 18 ? console.log("Si eres mayor de edad") : console.log("No eres mayor de edad")
+  }
+  
+  mayorEdad(15);
+  
+  
